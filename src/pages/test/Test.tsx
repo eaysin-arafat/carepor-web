b863d0cb4ec94180488f7aa499d2d6ba49319fee
developerachem <developerachem@gmail.com>
2023-11-27 17:33:33 +0600

    Complete Multi Step Concept

--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -1,17 +1,27 @@
 import Input from "@/components/core/form-elements/Input";
 import FormLayout from "@/components/core/form-layouts/FormLayout";
 import MultiStepComponent from "@/components/shared/multi-step/multiStep";
+import { useState } from "react";
 
 function Test() {
+  const [count, setCount] = useState(1);
   const stepTitle = [
     "Personal <br /> Information",
-    "Parents or <br /> Guardian Details",
-    "Parents or <br /> Guardian Details",
-    "Parents or <br /> Guardian Details",
-    "Parents or <br /> Guardian Details",
-    "Parents or <br /> Guardian Details",
-    "Parents or <br /> Guardian Details",
+    "Contact <br /> Information",
+    "Login <br /> Information",
+    "Login <br /> Information",
+    "Login <br /> Information",
+    "Login <br /> Information",
+    "Login <br /> Information",
   ];
+
+  const handleBack = () => {
+    setCount((prev) => prev - 1);
+  };
+  const handleNext = () => {
+    setCount((next) => next + 1);
+  };
+
   return (
     <div
       style={{
@@ -20,19 +30,39 @@ function Test() {
         margin: "auto",
       }}
     >
-      <MultiStepComponent active={1} title={stepTitle} />
+      <div className="w-[1000px] m-auto">
+        <MultiStepComponent active={count} title={stepTitle} />
+      </div>
       <FormLayout
-        className="min-w-[1300px]"
+        className="min-w-[1300px] mt-10"
         subTitle="Client Profile Registration"
       >
         <div className="mt-5">
-          <div className="grid grid-cols-2 gap-5">
-            <Input label="Signup" />
-            <Input label="Signup" />
-            <Input label="Signup" />
-            <Input label="Signup" />
-            <Input label="Signup" />
-            <Input label="Signup" />
+          {count === 1 && (
+            <div className="grid grid-cols-2 gap-5">
+              <Input label="Signup" />
+              <Input label="Signup" />
+              <Input label="Signup" />
+              <Input label="Signup" />
+              <Input label="Signup" />
+              <Input label="Signup" />
+            </div>
+          )}
+          {count === 2 && (
+            <div className="grid grid-cols-2 gap-5">
+              <Input label="Signup" />
+              <Input label="Signup" />
+            </div>
+          )}
+          <div className="flex gap-5">
+            <button className="btn bg-slate-500 mt-5" onClick={handleBack}>
+              {" "}
+              Back{" "}
+            </button>
+            <button className="btn bg-slate-500 mt-5" onClick={handleNext}>
+              {" "}
+              Next{" "}
+            </button>
           </div>
         </div>
       </FormLayout>
