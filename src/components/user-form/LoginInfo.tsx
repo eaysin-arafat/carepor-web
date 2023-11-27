fc7b7a0dd1f31b487abcff445266e04cd57160ae
amirhamja4bd <amirhamja4bd@gmail.com>
2023-11-27 23:14:07 +0600

    Code merge with Ananda Vai

--- src/components/user-form/LoginInfo.tsx
+++ src/components/user-form/LoginInfo.tsx
@@ -1,20 +1,26 @@
-import Input from "../form-elements/Input";
-import FormSection from "../form-elements/form-layouts/FormSection";
-import Password from "../form-elements/password";
-
 // type Props = {};
+
+import Input from "../core/form-elements/Input";
+import Password from "../core/form-elements/PasswordInput";
+import FormSection from "../core/form-layouts/FormSection";
 
 function LoginInfo() {
   return (
     <>
       <FormSection titleText="Login Information">
-        <div className="">
-          <Input required label="User Name" placeholder="Add User Name" />
-        </div>
-        <div className="grid md:grid-cols-2 grid-cols-1 gap-5 mt-4">
-          <Password required label="Password" placeholder="Password" />
-          <Password required label="Confirm Password" placeholder="Password" />
-        </div>
+        <>
+          <div className="">
+            <Input required label="User Name" placeholder="Add User Name" />
+          </div>
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-5 mt-4">
+            <Password required label="Password" placeholder="Password" />
+            <Password
+              required
+              label="Confirm Password"
+              placeholder="Password"
+            />
+          </div>
+        </>
       </FormSection>
     </>
   );
