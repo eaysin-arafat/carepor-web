c9f7d06a9db029333de0b388519dc71285dbfc11
amirhamja4bd <amirhamja4bd@gmail.com>
2023-11-28 11:23:34 +0600

    twmerge install

--- src/components/core/titles/Titles.tsx
+++ src/components/core/titles/Titles.tsx
@@ -7,28 +7,42 @@ type Props = {
 function Title({ title, className, type }: Props) {
   switch (type) {
     case "h1":
-      return (
-        <h1 className={`text-center font-bold text-2xl md:text-4xl text-heading leading-[1.625] tracking-tighter text-secondaryColor ${className}`}>{title}</h1>
-      );
+      return <h1 className={`heading_1 ${className}`}>{title}</h1>;
 
     case "h2":
       return (
-        <h2 className={`text-brand-heading text-3xl font-medium leading-normal tracking-tighter ${className}`}>{title}</h2>
+        <h2
+          className={`text-brand-heading text-3xl font-medium leading-normal tracking-tighter ${className}`}
+        >
+          {title}
+        </h2>
       );
 
     case "h3":
       return (
-        <h3 className={`text-secondaryColor text-lg font-bold leading-6 tracking-tighter ${className}`}>{title}</h3>
+        <h3
+          className={`text-secondaryColor text-lg font-bold leading-6 tracking-tighter ${className}`}
+        >
+          {title}
+        </h3>
       );
 
     case "span":
       return (
-        <span className={`text-center text-grayColor text-sm font-normal leading-6 ${className}`}>{title}</span>
+        <span
+          className={`text-center text-grayColor text-sm font-normal leading-6 ${className}`}
+        >
+          {title}
+        </span>
       );
 
     case "p":
       return (
-        <p className={`text-textColor font-inter font-normal text-sm leading-5 ${className}`}>{title}</p>
+        <p
+          className={`text-textColor font-inter font-normal text-sm leading-5 ${className}`}
+        >
+          {title}
+        </p>
       );
 
     default:
