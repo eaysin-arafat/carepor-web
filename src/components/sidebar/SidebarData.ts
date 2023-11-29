f40cc49a87fc5266d68de72954ad09b658a7ee49
developerachem <developerachem@gmail.com>
2023-11-29 18:36:07 +0600

    Update Sdebar Menu

--- src/components/sidebar/SidebarData.ts
+++ src/components/sidebar/SidebarData.ts
@@ -1,139 +1,193 @@
 const SidebarList = [
   {
+    id: 1,
     title: "Dashboard",
+
     link: "/",
     icon: "/public/assets/svg/vitals.svg",
   },
   {
+    id: 2,
     title: "Vital",
+
     link: "/",
     icon: "",
   },
   {
+    id: 3,
     title: "HTS",
+
     link: "/",
     icon: "",
   },
   {
+    id: 4,
     title: "Encounter (OPD)",
+
     link: "/",
     children: [
       {
+        id: 1,
         title: "HTS",
+
         link: "/",
         icon: "",
       },
       {
+        id: 2,
         title: "HTS",
+
         link: "/",
         icon: "",
       },
       {
+        id: 3,
         title: "HTS",
+
         link: "/",
         icon: "",
       },
     ],
   },
   {
+    id: 5,
     title: "PEP",
+
     link: "/",
     icon: "",
   },
   {
+    id: 6,
     title: "PrEP",
+
     link: "/",
     icon: "",
   },
   {
+    id: 7,
     title: "TB Service",
+
     link: "/",
     icon: "",
     children: [
       {
+        id: 1,
         title: "PEP",
+
         link: "/",
         icon: "",
       },
       {
+        id: 1,
         title: "PrEP",
+
         link: "/",
         icon: "",
       },
       {
+        id: 1,
         title: "TB Service",
+
         link: "/",
         icon: "",
       },
     ],
   },
   {
+    id: 8,
     title: "VMMC Service",
+
     link: "/",
     icon: "",
   },
   {
+    id: 9,
     title: "Under 5",
+
     link: "/",
     icon: "",
   },
   {
+    id: 10,
     title: "Pain Scaling Tool",
+
     link: "/",
     icon: "",
   },
   {
+    id: 11,
     title: "Investigation",
+
     link: "/",
     icon: "",
   },
   {
+    id: 12,
     title: "Surgery",
+
     link: "/",
     icon: "",
   },
   {
+    id: 13,
     title: "Referrals",
+
     link: "/",
     icon: "",
   },
   {
+    id: 14,
     title: "Covax",
+
     link: "/",
     icon: "",
   },
   {
+    id: 15,
     title: "Covid",
+
     link: "/",
     icon: "",
   },
   {
+    id: 16,
     title: "Birth Record",
+
     link: "/",
     icon: "",
   },
   {
+    id: 17,
     title: "Death Record",
+
     link: "/",
     icon: "",
   },
   {
+    id: 18,
     title: "Pharmacy",
+
     link: "/",
     icon: "",
   },
   {
+    id: 19,
     title: "ART (Pediatric)",
+
     link: "/",
     icon: "",
   },
   {
+    id: 20,
     title: "Postnatal (PNC)",
+
     link: "/",
     icon: "",
   },
   {
+    id: 22,
     title: "Family Planning",
+
     link: "/",
     icon: "",
   },
