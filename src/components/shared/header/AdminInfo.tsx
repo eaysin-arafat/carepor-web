cf675bfd875e2bc528532132eff2e8b26712abc8
dev-Anamul <ah9737677@gmail.com>
2023-12-02 16:44:59 +0600

    update facility name

--- src/components/shared/header/AdminInfo.tsx
+++ src/components/shared/header/AdminInfo.tsx
@@ -1,19 +1,31 @@
 import Title from "@/components/core/titles/Titles";
+import { logout } from "@/features/authentication/authentication-slice";
+import useFacility from "@/hooks/useFacility";
 import useWindowWidth from "@/hooks/useWindow";
+import { cookieManager } from "@/utilities/cookie-manager";
 import { useState } from "react";
 import { BsLock } from "react-icons/bs";
 import { CiSettings } from "react-icons/ci";
 import { FiEdit } from "react-icons/fi";
 import { IoExitOutline } from "react-icons/io5";
 import { MdOutlineDarkMode } from "react-icons/md";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 function AdminInfo() {
   // * Hooks
   const [adminBar, setAdminBar] = useState(false);
   const w1230 = useWindowWidth(1230);
-  const w1100 = useWindowWidth(1100);
   const w800 = useWindowWidth(800);
+  const dispatch = useDispatch();
+
+  const { facility } = useFacility();
+  console.log("facility header", facility);
+
+  const handleLogout = () => {
+    cookieManager.removeCookie("carepro_token");
+    dispatch(logout());
+  };
 
   return (
     <div className="flex items-center gap-2">
@@ -65,9 +77,12 @@ function AdminInfo() {
                 </Link>
               </li>
               <li>
-                <Link to="/" className="px-5 py-3 flex items-center gap-3">
+                <button
+                  className="px-5 py-3 flex items-center gap-3"
+                  onClick={handleLogout}
+                >
                   <IoExitOutline size={25} className="w-[30px]" /> Sign Out
-                </Link>
+                </button>
               </li>
             </ul>
           </div>
