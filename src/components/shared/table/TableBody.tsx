b6fdfed7e63441ce20e171f47140ce96216051c6
amirhamja4bd <amirhamja4bd@gmail.com>
2023-12-05 10:09:08 +0600

    Queues pages design

--- src/components/shared/table/TableBody.tsx
+++ src/components/shared/table/TableBody.tsx
@@ -1,36 +1,34 @@
 type Props = {
   index: number;
-  item: {
-    name: string;
-    age: string;
-    office: string;
-    position: string;
-    salary: string | number;
-  };
+  item: [any];
 };
 
 function TableBody({ item, index }: Props) {
-  return (
-    <div
-      key={index}
-      className={`grid grid-cols-6 items-center ${
-        index % 2 ? "bg-gray-200" : "bg-white"
-      }`}
-    >
-      <p className="p-2 text-textColor text-xs">{item.name}</p>
-      <p className="p-2 text-textColor text-xs">{item.age} years</p>
-      <p className="p-2 text-textColor text-xs">{item.office}</p>
-      <p className="p-2 text-textColor text-xs">{item.position}</p>
-      <p className="p-2 text-textColor text-xs">{item.salary}</p>
-      <p
-        className={`p-2 text-textColor text-xs sticky w-[100px] right-0 z-50 ${
-          index % 2 ? "bg-gray-200" : "bg-white"
-        }`}
-      >
-        Action
-      </p>
-    </div>
-  );
+  console.log(item);
+  item?.length > 0 &&
+    item.map((item, index) => {
+      return (
+        <div
+          key={index}
+          className={`grid grid-cols-6 items-center ${
+            index % 2 ? "bg-gray-200" : "bg-white"
+          }`}
+        >
+          <p className="p-2 text-textColor text-xs">25</p>
+          <p className="p-2 text-textColor text-xs">1 years</p>
+          <p className="p-2 text-textColor text-xs">3</p>
+          <p className="p-2 text-textColor text-xs">2</p>
+          <p className="p-2 text-textColor text-xs">1</p>
+          <p
+            className={`p-2 text-textColor text-xs sticky w-[100px] right-0 z-50 ${
+              index % 2 ? "bg-gray-200" : "bg-white"
+            }`}
+          >
+            Action
+          </p>
+        </div>
+      );
+    });
 }
 
 export default TableBody;
