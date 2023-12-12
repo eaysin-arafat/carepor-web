34cc70d3b99855e4c9c8bb4e33e83a605ab7356f
amirhamja4bd <amirhamja4bd@gmail.com>
2023-12-12 10:02:27 +0600

    Investigation form

--- src/pages/investigations/index/Investigation.tsx
+++ src/pages/investigations/index/Investigation.tsx
@@ -33,7 +33,7 @@ const Investigation = () => {
     <>
       <InvestigationCreate />
       <div className={cn("", { "mt-12": w1100 })}>
-        <InvestigationCreateForm/>
+        {/* <InvestigationCreateForm/> */}
         <ClientDetailsCard className="shadow-none" />
         <div className="grid grid-cols-4 gap-5 mt-5">
           <div className="col-span-4 lg:col-span-3">
